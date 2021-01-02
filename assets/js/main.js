73aabdfc2a8e9c3ab0c9dfd793262c3bcb04bb2e
Pure KaoZ <nickolaus.carter81@gmail.com>
2021-01-02 11:37:41 -0600

    game page style

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+const questionEl = document.querySelector('#question');
+const choicesEl = Array.from(document.querySelectorAll('.choice-text'));
+const progressTextEl = document.querySelector('#progress-text');
+const scoreTextEl = document.querySelector('#score');
+const progressBarFullEl = document.querySelector('#progress-bar-full');
